28190d2f383b3242ff9c1265b9127c1fe9f2a4bd
danielraja2000 <65912048+danielraja2000@users.noreply.github.com>
2020-05-29 10:24:38 +0530

    Add files via upload

---
+++ day 3 remaining  prog/OneToTen.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class OneToTen 
+{
+    public static void main(String[] args) 
+    {
+        for(int i = 1; i <= 10; i++) 
+        {
+            System.out.print(" " + i);
+        }
+    }
+}

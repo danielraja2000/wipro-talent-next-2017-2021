4cadadc7c21aeedfcc7fd95d777a4630968c7c02
danielraja2000 <65912048+danielraja2000@users.noreply.github.com>
2020-06-02 17:15:55 +0530

    Add files via upload

---
+++ Day 6/ListProg6.java
@@ -0,0 +1,34 @@
+import java.util.LinkedList;
+public class ListProg6 {
+	private LinkedList<Integer> list = new LinkedList<Integer>();
+	
+	public LinkedList<Integer> saveEvenNumbers(int N) {
+		list = new LinkedList<Integer>();
+		
+		for (int i = 2; i <= N; i++) {
+			if (i % 2 == 0) list.add(i);
+		}
+		
+		return list;
+	}
+	
+	public LinkedList<Integer> printEvenNumbers() {
+		LinkedList<Integer> newList = new LinkedList<Integer>();
+		
+		for (int item : list) {
+			newList.add(item * 2);
+			System.out.println(item * 2);
+		}
+		
+		return newList;
+	}
+
+	public static void main(String[] args) {
+		ListProg1 a = new Assignment1();
+		a.saveEvenNumbers(10);
+		a.printEvenNumbers();
+		
+
+	}
+
+}
---
+++ Day 6/ListProg7.java
@@ -0,0 +1,34 @@
+import java.util.Vector;
+public class ListProg7 {
+	private Vector<Integer> list = new Vector<Integer>();
+	
+	public Vector<Integer> saveEvenNumbers(int N) {
+		list = new Vector<Integer>();
+		
+		for (int i = 2; i <= N; i++) {
+			if (i % 2 == 0) list.add(i);
+		}
+		
+		return list;
+	}
+	
+	public Vector<Integer> printEvenNumbers() {
+		Vector<Integer> newList = new Vector<Integer>();
+		
+		for (int item : list) {
+			newList.add(item * 2);
+			System.out.println(item * 2);
+		}
+		
+		return newList;
+	}
+
+	public static void main(String[] args) {
+		ListProg1 a = new Assignment1();
+		a.saveEvenNumbers(10);
+		a.printEvenNumbers();
+		
+
+	}
+
+}
